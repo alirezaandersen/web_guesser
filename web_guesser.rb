dfa3568ad5dd1f64cccb1eb414981e98cae0198b
Alireza Andersen <alireza.andersen@gmail.com>
2016-03-11 11:54:17 -0700

    working game with colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,11 +1,63 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
+
+SECRETNUMBER = rand(100)
 
 
 get '/' do
-  number = rand(4)
-  erb :index, :locals => {:number => number}
-
+  num = params["guess"].to_i
+  message = check_guess(num)
+  background_color = background_color(num)
+  erb :index, :locals => {:number => SECRETNUMBER, :message => message, :background_color => background_color}
 end
+
+def check_guess(num)
+  if num > SECRETNUMBER + 5
+    'Your guess is WAY TO HIGH!'
+  elsif num > SECRETNUMBER
+    'guess is to high'
+  elsif num < SECRETNUMBER - 5
+      'Your guess is WAY TO LOW!'
+  elsif num < SECRETNUMBER
+      'guess is to low'
+  else
+    num == SECRETNUMBER
+    "You got it correctly \n The SECRET NUMBER is #{SECRETNUMBER}"
+  end
+end
+
+
+def background_color(num)
+  if num > SECRETNUMBER + 5
+    'red'
+  elsif num > SECRETNUMBER
+    'blue'
+  elsif num < SECRETNUMBER - 5
+      'green'
+  elsif num < SECRETNUMBER
+      'yello'
+  else
+    num == SECRETNUMBER
+    "purple"
+  end
+end
+# def way_to_high(num)
+#   if num > SECRETNUMBER + 5
+#     'Your guess is WAY TO HIGH!'
+#   elsif num > SECRETNUMBER
+#     'guess is to high'
+#   end
+# end
+#
+# def way_to_low(num)
+#   if num < SECRETNUMBER - 5
+#     'Your guess is WAY TO LOW!'
+#   elsif num > SECRETNUMBER
+#     'guess is to low'
+#   end
+# end
+
+
 # erb :index, :locals => {:number => number}
